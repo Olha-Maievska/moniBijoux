818460daf25bb76bd56adc2e7a1b1e5459f77ec1
Olha Maievska <olhamaievska91@gmail.com>
2024-08-02 12:57:13 +0200

    added project

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const slider__wrapper = document.querySelector('#reviews__wrapper')
+  const btnPrev = document.querySelector('#prev-btn')
+  const btnNext = document.querySelector('#next-btn')
+});
